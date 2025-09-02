d56c3e0657a900c57c20e1a2c1f6de46099e94dd
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2025-09-02 18:02:16 +0300

    Joomla 6 plugin creation convention
    Future-proofing.

--- plugins/system/socialmagick/services/provider.php
+++ plugins/system/socialmagick/services/provider.php
@@ -7,6 +7,7 @@
 
 defined('_JEXEC') || die;
 
+use Akeeba\Plugin\System\SocialMagick\Extension\SocialMagick;
 use Joomla\CMS\Extension\PluginInterface;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
@@ -14,26 +15,24 @@ use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
-use Akeeba\Plugin\System\SocialMagick\Extension\SocialMagick;
 
 return new class implements ServiceProviderInterface {
-	/**
-	 * Registers the service provider with a DI container.
-	 *
-	 * @param   Container  $container  The DI container.
-	 *
-	 * @return  void
-	 *
-	 * @since   2.0.0
-	 */
 	public function register(Container $container)
 	{
 		$container->set(
 			PluginInterface::class,
 			function (Container $container) {
-				$subject = $container->get(DispatcherInterface::class);
 				$config  = (array) PluginHelper::getPlugin('system', 'socialmagick');
-				$plugin  = new SocialMagick($subject, $config);
+
+				if (version_compare(JVERSION, '5.999.999', 'gt'))
+				{
+					$plugin  = new SocialMagick($config);
+				}
+				else
+				{
+					$subject = $container->get(DispatcherInterface::class);
+					$plugin  = new SocialMagick($subject, $config);
+				}
 
 				$plugin->setApplication(Factory::getApplication());
 				$plugin->setDatabase($container->get(DatabaseInterface::class));
